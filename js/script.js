9c8bee5f7ce93d542526c06dc7c198bfb6076975
tpo <thepaut.emma@gmail.com>
2019-02-13 11:30:41 +0100

    init

--- js/script.js
+++ js/script.js
@@ -66,3 +66,17 @@ $(".arrow").on("click", function(){
         }
         showSlide(slideIndex)};
 })
+
+$(function() {
+    var selectedClass = "";
+    $(".fil-cat").click(function(){ 
+    selectedClass = $(this).attr("data-rel"); 
+ $("#portfolio").fadeTo(100, 0.1);
+    $("#portfolio div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
+setTimeout(function() {
+  $("."+selectedClass).fadeIn().addClass('scale-anm');
+  $("#portfolio").fadeTo(300, 1);
+}, 300); 
+    
+});
+});
